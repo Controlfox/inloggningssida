7716b6be3a4f2049849b5f90749cfff30d02bd93
Controlfox <thlm@live.se>
2024-09-09 12:53:10 +0200

    Lagt till inputs och inloggningsknapp

---
+++ script.js
@@ -0,0 +1,7 @@
+const namnInput = document.createElement("input");
+const lösenInput = document.createElement("input");
+const inloggBtn = document.createElement("button");
+
+inloggBtn.innerText = "Logga in";
+
+document.body.append(namnInput, lösenInput, inloggBtn);

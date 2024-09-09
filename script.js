5da1ebef42cf9cad75476f9ad792f02847847bd5
Controlfox <thlm@live.se>
2024-09-09 13:23:13 +0200

    Lagt till paragrafer

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const namnInput = document.createElement("input");
 const lösenInput = document.createElement("input");
 const inloggBtn = document.createElement("button");
+const pNamn = document.createElement("p");
+const pLösen = document.createElement("p");
 
 inloggBtn.innerText = "Logga in";
+pNamn.innerText = "Användarnamn";
+pLösen.innerText = "Lösenord";
 
-document.body.append(namnInput, lösenInput, inloggBtn);
+document.body.append(pNamn,namnInput,pLösen,lösenInput,inloggBtn);
